75bb6fa08519dc5ff59005b24c401083be4abea9
James Sungjae Lee <sungjae@kookmin.ac.kr>
2022-10-17 08:45:30 +0900

    spot-score to spotlake

--- collection/aws/ec2_collector/load_price.py
+++ collection/aws/ec2_collector/load_price.py
@@ -11,7 +11,7 @@ from load_metadata import get_regions
 
 
 BUCKET_NAME = 'spotlake'
-LOCAL_PATH = '/home/ubuntu/spot-score/collection/aws/ec2_collector'
+LOCAL_PATH = '/home/ubuntu/spotlake/collection/aws/ec2_collector'
 
 
 # get spot price by all availability zone in single region

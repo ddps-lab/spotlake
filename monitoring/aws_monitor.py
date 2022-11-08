93e54db9d91c1ff698e1af36dd3b9e2593d658c9
red0sena <bryan9801@kookmin.ac.kr>
2022-11-08 12:16:03 +0900

    update aws_monitor.py timestream query to athena query

--- monitoring/aws_monitor.py
+++ monitoring/aws_monitor.py
@@ -1,53 +1,67 @@
-from datetime import datetime, timedelta
 import boto3
+import time
 import requests
-import json
-import pickle
 import io
+import pickle
+import json
+from datetime import datetime, timedelta
 
-session = boto3.Session()
-query_client = session.client('timestream-query')
-client = query_client
-daily_count = 0
-result_msg = ""
-start_date = (datetime.today().date() + timedelta(days=-1)).strftime('%Y-%m-%d %H:%M:%S')
-end_date = (datetime.today().date() + timedelta(days=0)).strftime('%Y-%m-%d %H:%M:%S')
-s3_path_date = (datetime.today().date() + timedelta(days=0)).strftime('%Y/%m/%d')
+
+bucket_name = 'spotlake'
+s3_path_date = (datetime.today().date() + timedelta(days=-1)).strftime('%Y/%m/%d')
 s3 = boto3.client('s3')
-bucket_name = '' #bucket_name
+athena_handler = boto3.client('athena',region_name='us-west-2')
 
 
-# generate query_string and get the count of yesterday data from Timestream.
-def run_query():
-    try:
-        query_string = f"""SELECT count(*) FROM "spotrank-timestream"."spot-table" WHERE time between timestamp '{start_date}' and timestamp '{end_date}' """
-        response = client.query(QueryString=query_string)
-        result = response["Rows"]
-        while 'NextToken' in response:
-            response = client.query(QueryString=query_string, NextToken=response['NextToken'])
-            result.extend(response["Rows"])
-        if result == []:
-            daily_count = 0
-        else:
-            daily_count = result[0]['Data'][0]['ScalarValue']
-        num_of_workload = get_workload_num()
-        send_message(daily_count, num_of_workload)
-    except Exception as err:
-        print("Exception while running query:", err)
+def create_athena_table():
+    athena_handler.start_query_execution(
+        QueryString=f"""
+             CREATE EXTERNAL TABLE IF NOT EXISTS aws (
+                instancetype string,
+                az string,
+                spotprice double,
+                region string,
+                sps int,
+                ondemandprice double,
+                interruptfrequency double,
+                savings int
+              ) 
+              ROW FORMAT DELIMITED
+              FIELDS TERMINATED BY ','
+              LINES TERMINATED BY '\n'
+              LOCATION 's3://spotlake/rawdata/aws/{s3_path_date}'
+              TBLPROPERTIES ("skip.header.line.count"="1"); 
+             """,
+        ResultConfiguration={'OutputLocation': f's3://spotlake-count-log/create-athena-table/{s3_path_date}/'}
+    )
+    print("create table")
 
 
-# send data count message to slack
-def send_message(count, num_of_workload):
-    global result_msg
-    url=''#slackurl
-    result_msg = """<%s spotlake_workload_monitoring> \n- The number of ingested records. : %s\n- The number of records that must be ingested. : %d""" % (datetime.today().date(), count, num_of_workload * 144)
-    data = {'text': result_msg}
-    resp = requests.post(url=url, json=data)
-    return resp
 
-# get number of all Region-InstanceType-AvailabilityZone from s3 pkl file
-# get the instancetype and pair of region-az_num from the pickle file, assign the num of AZ in azs, and return it.
-# The workload is a combine of Region-InstanceType-AvailabilityZone
+def query_athena(query_string):
+    query_start = athena_handler.start_query_execution(
+        QueryString=query_string,
+        ResultConfiguration={'OutputLocation': f's3://spotlake-count-log/query_athena/{s3_path_date}/'}
+    )
+
+    while True:
+        query_status = athena_handler.get_query_execution(
+            QueryExecutionId=query_start['QueryExecutionId'])
+        query_execution_status = query_status['QueryExecution']['Status'][
+            'State']
+
+        if query_execution_status == 'SUCCEEDED':
+            result = athena_handler.get_query_results(
+                QueryExecutionId=query_start['QueryExecutionId'])
+            break
+
+        print("sleep")
+        time.sleep(1)
+
+    print(result)
+    return result["ResultSet"]['Rows'][1]['Data'][0]['VarCharValue']
+
+
 def get_workload_num():
     try:
         obj = s3.get_object(Bucket=bucket_name, Key=f'monitoring/{s3_path_date}/workloads.pkl')
@@ -60,10 +74,36 @@ def get_workload_num():
     except Exception as e:
         print(e)
 
+
+def send_message(sps, interruptfrequency, spot, ondemand, num_of_workloads):
+    url = '' #slackurl
+    result_msg = f"""
+    <{datetime.today().date()} spotlake_workload_monitoring>\n
+    - The number of ingested records.\n
+        - SPS : {sps}
+        - IF  : {int(interruptfrequency)*144}
+        - spot: {spot}
+        - ondemand : {ondemand}
+    - The number of records that must be ingested. : {num_of_workloads*144}"""
+    data = {'text': result_msg}
+    resp = requests.post(url=url, json=data)
+    return data
+
+
 def lambda_handler(event, context):
-    global result_msg
-    run_query()
+    create_athena_table()
+
+    sps = query_athena('SELECT count(*)FROM "default"."aws" where sps != -1;')
+    interruptfrequency = query_athena('SELECT COUNT(DISTINCT(instancetype, region)) FROM "default"."aws" WHERE interruptfrequency != -1')
+    spotprice = query_athena('SELECT count(*)FROM "default"."aws" where spotprice != -1;')
+    ondemandprice = query_athena('SELECT count(*)FROM "default"."aws" where ondemandprice != -1;')
+    num_of_workload = get_workload_num()
+
+    message = send_message(sps, interruptfrequency, spotprice, ondemandprice, num_of_workload)
+
     return {
         'statusCode': 200,
-        'body': json.dumps(result_msg)
+        'body': json.dumps(message)
     }
+
+

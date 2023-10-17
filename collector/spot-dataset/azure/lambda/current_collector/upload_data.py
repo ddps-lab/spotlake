ba2a005ab3f21bda2c8092f26adae86187f11ea8
red0sena <bryan9801@kookmin.ac.kr>
2023-10-17 16:22:33 +0900

    update azure collector fix regioin column

--- collector/spot-dataset/azure/lambda/current_collector/upload_data.py
+++ collector/spot-dataset/azure/lambda/current_collector/upload_data.py
@@ -111,7 +111,7 @@ def update_latest(data, timestamp):
 # Save raw data in S3
 def save_raw(data, timestamp):
     data['Time'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-    data = data[['Time','InstanceTier','InstanceType','OndemandPrice','SpotPrice', 'IF', 'Savings']]
+    data = data[['Time','InstanceTier','InstanceType', 'Region', 'OndemandPrice','SpotPrice', 'IF', 'Savings']]
 
     data.to_csv(f"{AZURE_CONST.SERVER_SAVE_DIR}/{timestamp}.csv.gz", index=False, compression="gzip")
 

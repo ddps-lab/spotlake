a9f3a3b5017d97c0c22660465b65e0132083c9b7
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-02-26 14:16:49 +0900

    refractored_v1

--- collector/spot-dataset/aws/sps_collector/main.py
+++ collector/spot-dataset/aws/sps_collector/main.py
@@ -8,7 +8,7 @@ import os
 import gzip
 from datetime import datetime
 from sps_query_api import query_sps
-from sps_utils import upload_data_to_s3, log_execution_time, calculate_execution_ms, upload_log_event
+from sps_utils import *
 from time import time
 
 from concurrent.futures import ThreadPoolExecutor
@@ -18,119 +18,110 @@ sys.path.append("/home/ubuntu/spotlake/utility")
 from slack_msg_sender import send_slack_message
 
 s3 = boto3.resource("s3")
+s3_client = boto3.client('s3')
 log_client = boto3.client('logs')
+
 NUM_WORKER = 26
 CURRENT_PATH = "/home/ubuntu/spotlake/collector/spot-dataset/aws/sps_collector/"
 WORKLOAD_FILE_PATH = "rawdata/aws/workloads"
 CREDENTIAL_FILE_PATH = "credential/credential_3699.csv"
-START_CREDENTIAL_INDEX = 100
-LOG_GROUP_NAME = "SPS_Collected-Data"
-LOG_STREAM_NAME_EXECUTION_TIME = "execution-time"
-LOG_STREAM_NAME_AMOUNT_DATA = "amount-data"
+BUCKET_NAME = "sps-query-test" #test
+WORKLOAD_BUCKET_NAME = "spotlake"
 
-# test
-bucket_name = "sps-query-test"
-workload_bucket_name = "spotlake"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--timestamp', dest='timestamp', action='store')
 args = parser.parse_args()
 timestamp_utc = datetime.strptime(args.timestamp, "%Y-%m-%dT%H:%M")
 date = args.timestamp.split("T")[0]
-
 rounded_minute = (timestamp_utc.minute // 10) * 10 # 분을 10분단위로 내림합니다.
 timestamp_utc = timestamp_utc.replace(minute=rounded_minute, second=0)
-S3_DIR_NAME = timestamp_utc.strftime("%Y/%m/%d/%H/%M")
+S3_DIR_NAME = timestamp_utc.strftime("%Y/%m/%d/")
+S3_OBJECT_PREFIX = timestamp_utc.strftime("%H-%M")
+print(S3_DIR_NAME)
+print(S3_OBJECT_PREFIX)
 
 total_execution_time_ms = 0
-target_capacities = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
-idx_credential = START_CREDENTIAL_INDEX
-def get_work_per_thread():
-    global idx_credential
-    work_per_thread = []
-    for target_capacity in target_capacities:
-        work_per_target_capacity = []
-        for scenarios in workload:
-            credential = credentials.iloc[idx_credential]
-            idx_credential += 1
-            work_per_target_capacity.append((credential, scenarios, target_capacity))
-        work_per_thread.append(work_per_target_capacity)
-    return work_per_thread
 
+# ------ Load Workload File -------
 start_time = time()
 workload = None
 try:
     key = f"{WORKLOAD_FILE_PATH}/{'/'.join(date.split('-'))}/binpacked_workloads.pkl.gz"
-    workload = pickle.load(gzip.open(s3.Object(workload_bucket_name, key).get()["Body"]))
+    workload = pickle.load(gzip.open(s3.Object(WORKLOAD_BUCKET_NAME, key).get()["Body"]))
+    s3.download_file(WORKLOAD_BUCKET_NAME, key, "binpacked_workloads.pkl.gz")
 except Exception as e:
-    send_slack_message(e)
-    raise e
+    message = f"bucket : {WORKLOAD_BUCKET_NAME}, object : {key} 가 수집되지 않았습니다.\n 서버에 있는 로컬 workload파일을 불러옵니다."
+    send_slack_message(message)
+    try:
+        with gzip.open(f"{CURRENT_PATH}binpacked_workloads.pkl.gz", 'rb') as f:
+            workload = pickle.load(f)
+    except:
+        message = f"로컬파일에 workload파일이 존재하지 않습니다."
+        send_slack_message(message)
+        raise e
 end_time = time()
 total_execution_time_ms += calculate_execution_ms(start_time, end_time)
 
+# ------ Load Credential File ------
 start_time = time()
 credentials = None
 try:
-    csv_content = s3.Object(bucket_name, CREDENTIAL_FILE_PATH).get()["Body"].read().decode('utf-8')
+    csv_content = s3.Object(BUCKET_NAME, CREDENTIAL_FILE_PATH).get()["Body"].read().decode('utf-8')
     credentials = pd.read_csv(StringIO(csv_content))
 except Exception as e:
     send_slack_message(e)
     raise e
 end_time = time()
 total_execution_time_ms += calculate_execution_ms(start_time, end_time)
-    
-start_time = time()
-work_per_thread = get_work_per_thread()
-end_time = time()
-total_execution_time_ms += calculate_execution_ms(start_time, end_time)
 
-while True:
-    try:
-        sps_df_per_target_capacity = []
-        idx_target_capacity = 0 # 출력용 변수입니다.
-        for work_per_target_capacity in work_per_thread:
-            target_capacity = target_capacities[idx_target_capacity]
+target_capacities = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+current_credential_index = 0
+sps_df_list = []
+for target_capacity in target_capacities:
+    # ------ Start Query Per Target Capacity ------
+    while True:
+        try:
             start_time = time()
+            works = []
+            start_credential_index = current_credential_index
+            for scenarios in workload:
+                works.append((credentials.iloc[current_credential_index], scenarios, target_capacity))
+                current_credential_index += 1
+            end_credential_index = current_credential_index
             with ThreadPoolExecutor(max_workers=NUM_WORKER) as executor:
-                sps_df_list = list(executor.map(query_sps, work_per_target_capacity))
-            df_combined = pd.concat(sps_df_list, axis=0, ignore_index=True)
-            sps_df_per_target_capacity.append(df_combined)
+                df_list = list(executor.map(query_sps, works))
+            df_combined = pd.concat(df_list, axis=0, ignore_index=True)
+            sps_df_list.append(df_combined)
             end_time = time()
-            total_execution_time_ms += calculate_execution_ms(start_time, end_time)
-            response = log_execution_time(log_client, start_time, end_time, 
-                                            LOG_GROUP_NAME, LOG_STREAM_NAME_EXECUTION_TIME, 
-                                            f"QUERY_TIME_{target_capacity}")
-            idx_target_capacity += 1
-        break
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'MaxConfigLimitExceeded':
-            work_per_thread = get_work_per_thread()
-        else:
+            log_extra(log_client, f"사용한 credential range : ({start_credential_index}, {end_credential_index})")
+            log_execution_time(log_client, start_time, end_time, f"QUERY_{target_capacity}")
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'MaxConfigLimitExceeded':
+                continue
+            else:
+                send_slack_message(e)
+                raise e
+        except Exception as e:
             send_slack_message(e)
             raise e
-    except Exception as e:
-        send_slack_message(e)
-        raise e
-
+        
+# ------ Merge Horizontally Collected DataFrame ------
 start_time = time()
 try:
     key = ['InstanceType', 'Region', 'AZ']
     merged_df = pd.DataFrame(columns=key)
-    for df in sps_df_per_target_capacity:
+    for df in sps_df_list:
         merged_df = pd.merge(merged_df, df, on=key, how='outer')
-    
-    csv_object_name = "sps_1_to_50.csv.gz"
-    SAVED_FILENAME = f"{CURRENT_PATH}"+f"{csv_object_name}"
-    merged_df.to_csv(SAVED_FILENAME, index=False, compression="gzip")
-    upload_data_to_s3(SAVED_FILENAME, S3_DIR_NAME, csv_object_name, bucket_name)
+    object_name = f"{S3_OBJECT_PREFIX}_sps_1_to_50.csv.gz"
+    saved_filename = f"{CURRENT_PATH}"+f"{object_name}"
+    merged_df.to_csv(saved_filename, index=False, compression="gzip")
+    upload_data_to_s3(s3_client, saved_filename, S3_DIR_NAME, object_name, BUCKET_NAME)
 except Exception as e:
     send_slack_message(e)
     raise e
 end_time = time()
 total_execution_time_ms += calculate_execution_ms(start_time, end_time)
-response = log_execution_time(log_client, start_time, end_time, 
-                                LOG_GROUP_NAME, LOG_STREAM_NAME_EXECUTION_TIME, 
-                                f"TOTAL_EXECUTION_TIME")
-response = upload_log_event(log_client, LOG_GROUP_NAME, LOG_STREAM_NAME_AMOUNT_DATA,
-                            f"NUMBER_ROWS", merged_df.shape[0])
+log_execution_time(log_client, 0, total_execution_time_ms, "TOTAL_EXECUTION_TIME")
+log_amount(log_client, merged_df.shape[0])
--- collector/spot-dataset/aws/sps_collector/sps_utils.py
+++ collector/spot-dataset/aws/sps_collector/sps_utils.py
@@ -6,23 +6,24 @@ from datetime import datetime
 sys.path.append("/home/ubuntu/spotlake/utility")
 from slack_msg_sender import send_slack_message
 
+LOG_GROUP_NAME = "SPS-Collected-Data"
+LOG_STREAM_NAME_AMOUNT_DATA = "amount-data"
+LOG_STREAM_NAME_EXECUTION_TIME = "execution-time"
+LOG_STREAM_NAME_EXTRA = "extra-log"
+
 def calculate_execution_ms(start_time, end_time):
     return int((end_time - start_time) * 1000)
 
-def upload_data_to_s3(saved_filename, s3_dir_name, s3_obj_name, bucket_name):
-    session = boto3.Session()
-    s3 = session.client('s3')
-
+def upload_data_to_s3(s3_client, saved_filename, s3_dir_name, s3_obj_name, bucket_name):
     with open(saved_filename, 'rb') as f:
-        s3.upload_fileobj(f, bucket_name, f"aws/{s3_dir_name}/{s3_obj_name}")
+        s3_client.upload_fileobj(f, bucket_name, f"aws/{s3_dir_name}/{s3_obj_name}")
     
     os.remove(saved_filename)
     
-def upload_log_event(client, log_group_name, log_stream_name, log_event_key, log_event_value):
-    message = {log_event_key : log_event_value}
+def upload_log_event(log_client, log_group_name, log_stream_name, message):
     timestamp = int(datetime.utcnow().timestamp() * 1000)
     try:
-        response = client.put_log_events(
+        response = log_client.put_log_events(
             logGroupName=log_group_name,
             logStreamName=log_stream_name,
             logEvents=[
@@ -36,10 +37,25 @@ def upload_log_event(client, log_group_name, log_stream_name, log_event_key, log
         raise e
     return response
 
-def log_execution_time(client, start_time, end_time, log_group_name, log_stream_name, log_event_key):
+def log_execution_time(client, start_time, end_time, message_key):
     execution_time = calculate_execution_ms(start_time, end_time)
     try:
-        response = upload_log_event(client, log_group_name, log_stream_name, log_event_key, execution_time)
+        response = upload_log_event(client, LOG_GROUP_NAME, LOG_STREAM_NAME_EXECUTION_TIME, f"{message_key} {execution_time}")
+    except Exception as e:
+        raise e
+    return response
+
+def log_extra(client, message):
+    try:
+        response = upload_log_event(client, LOG_GROUP_NAME, LOG_STREAM_NAME_EXTRA, message)
+    except Exception as e:
+        raise e
+    return response
+
+def log_amount(client, amount_data):
+    message = f"DATA_ROWS {amount_data}"
+    try:
+        response = upload_log_event(client, LOG_GROUP_NAME, LOG_STREAM_NAME_AMOUNT_DATA, message)
     except Exception as e:
         raise e
     return response

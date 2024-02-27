1b0bad0275d9084c5b99d54882f78cb89195dcb2
Ubuntu <ubuntu@ip-172-31-29-65.us-west-2.compute.internal>
2024-02-27 10:29:32 +0000

    End last test

--- collector/spot-dataset/aws/sps_collector/main.py
+++ collector/spot-dataset/aws/sps_collector/main.py
@@ -34,6 +34,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--timestamp', dest='timestamp', action='store')
 args = parser.parse_args()
 timestamp_utc = datetime.strptime(args.timestamp, "%Y-%m-%dT%H:%M")
+
+print(f"스크립트 실행 시작 시간 (UTC) : {timestamp_utc}")
+
 date = args.timestamp.split("T")[0]
 rounded_minute = (timestamp_utc.minute // 10) * 10 # 분을 10분단위로 내림합니다.
 timestamp_utc = timestamp_utc.replace(minute=rounded_minute, second=0)
@@ -61,23 +64,22 @@ try:
         with open(CREDENTIAL_START_INDEX_FILE_NAME, 'w') as f:
             f.write(str(current_credential_index))
 except Exception as e:
-    message = f"bucket : {WORKLOAD_BUCKET_NAME}, object : {key} 가 수집되지 않았습니다.\n 서버에 있는 로컬 workload파일을 불러옵니다."
+    message = f"bucket : {WORKLOAD_BUCKET_NAME}, object : {key} 가 수집되지 않았습니다.\n서버에 있는 로컬 workload파일을 불러옵니다."
     send_slack_message(message)
-    try:
-        is_local = False
-        for filename in os.listdir(f"{CURRENT_PATH}"):
-            if "_binpacked_workloads.pkl.gz" in filename:
-                print(f"로컬 워크로드 파일 {filename} 사용")
-                with open(f"{CURRENT_PATH}{filename}", 'rb') as f:
-                    workload = pickle.load(f)
-                is_local = True
-                break
-        if not is_local:
-            raise Exception("does not exist local workloads file")
-    except Exception as another_exception:
+    is_local = False
+    for filename in os.listdir(f"{CURRENT_PATH}"):
+        if "_binpacked_workloads.pkl.gz" in filename:
+            print(f"로컬 워크로드 파일 {CURRENT_PATH}{filename} 사용")
+            with open(f"{CURRENT_PATH}{filename}", 'rb') as f:
+                workload = pickle.load(gzip.open(f))
+            is_local = True
+            break
+    if not is_local:
         message = f"로컬파일에 workload파일이 존재하지 않습니다."
         send_slack_message(message)
-        raise e
+        raise Exception("does not exist local workloads file")
+
+print(f"계정 시작 인덱스 : {current_credential_index}")
 
 # ------ Load Credential File ------
 credentials = None
@@ -107,9 +109,9 @@ for target_capacity in target_capacities:
             df_combined = pd.concat(df_list, axis=0, ignore_index=True)
             sps_df_list.append(df_combined)
             end_time = time()
-            message = f"사용한 credential range : ({start_credential_index}, {end_credential_index})"
+            message = f"Target Capacity {target_capacity} Query Time : {calculate_execution_ms(start_time, end_time)} ms"
+            message += f"\n사용한 credential range : ({start_credential_index}, {end_credential_index})"
             print(message)
-            log_execution_time(log_client, start_time, end_time, f"QUERY_{target_capacity}")
             break
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == 'MaxConfigLimitExceeded':
@@ -135,9 +137,12 @@ except Exception as e:
     send_slack_message(e)
     raise e
 
+# ------ Monitoring for total execution time ------
 execution_time_end = time()
 total_execution_time = calculate_execution_ms(execution_time_start, execution_time_end)
 if total_execution_time >= 600000:
-    send_slack_message(f"sps 쿼리 시간이 10분을 초과하였습니다 : {total_execution_time} ms")
-log_execution_time(log_client, execution_time_start, execution_time_end, "TOTAL_EXECUTION_TIME")
-log_amount(log_client, merged_df.shape[0])
+    message = f"sps 쿼리 시간이 10분을 초과하였습니다 : {total_execution_time} ms"
+    message += f"\n실행 시작 시간 (UTC) : {timestamp_utc}"
+    send_slack_message(message)
+print(f"스크립트 실행 시간 : {total_execution_time} ms")
+print(f"수집된 DataFrame 행 수 : {merged_df.shape[0]}")

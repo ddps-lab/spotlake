16a59c81a1bedd466efe67d6650a0c03b2478131
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-04-11 17:22:20 +0900

    add monitoring code and gitignore

--- collector/spot-dataset/aws/sps_collector/monitoring/lambda_function.py
+++ collector/spot-dataset/aws/sps_collector/monitoring/lambda_function.py
@@ -1,6 +1,6 @@
+import boto3
 import sys
 import time
-import boto3
 import botocore
 import json
 from datetime import datetime, timedelta, timezone
@@ -8,10 +8,12 @@ from datetime import datetime, timedelta, timezone
 from slack_msg_sender import send_slack_message
 
 S3_PATH_DATE = (datetime.now(timezone.utc).date() + timedelta(days=-1)).strftime("%Y/%m/%d")
+
 S3_LOG_BUCKET = "sps-athena-log"
 TABLE_NAME = "sps_server_monitor"
 
 def query_athena(athena_client, query_string, result_configuration):
+    print(f"---- will query ----\n{query_string}")
     try:
         query_start = athena_client.start_query_execution(
             QueryString=query_string,
@@ -47,7 +49,7 @@ def query_athena(athena_client, query_string, result_configuration):
             """
             raise Exception(send_slack_message(message))
 
-        print(f"sleep {2**retries} seconds")
+        print(f"sleep {2**retries} seconds for waiting query")
         time.sleep(2**retries)
         retries += 1
     
@@ -85,29 +87,41 @@ def create_athena_table(athena_client, data_bucket_name):
     try:
         result = query_athena(athena_client, query_string, result_configuation)
     except Exception as e:
+        send_slack_message(f"exception at aws sps server monitoring create_athena_table()\n{query_string}\n--- error msg ---\n{e}")
         raise e
     print(f"create table {TABLE_NAME}")
 
 def drop_athena_table(athena_client):
-    query_string = f"DROP TABLE IF EXIST {TABLE_NAME}"
+    query_string = f"DROP TABLE IF EXISTS {TABLE_NAME}"
     result_configuration = {'OutputLocation': f"s3://{S3_LOG_BUCKET}/drop-athena-table/{S3_PATH_DATE}/"}
     try:
         result = query_athena(athena_client, query_string, result_configuration)
     except Exception as e:
+        print(f"The error message : {e}")
         raise e
     
 def make_message(athena_client):
-    sps_query_string = f"SELECT COUNT(*) FROM 'default'.'{TABLE_NAME}'"
+    target_capacities = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     result_configuration = {'OutputLocation': f's3://{S3_LOG_BUCKET}/check-table/{S3_PATH_DATE}/'}
-    result_sps = query_athena(athena_client, sps_query_string, result_configuration)
-    
-    message = f"""<{datetime.today().date()} sps server monitoring - AWS>
-    - the number of row collected sps data : {result_sps}"""
+    message = f"<{datetime.today().date()} sps server monitoring - AWS>"
+    message += f"\n```- the number of collected sps data per Target Capacity"
+
+    for target_capacity in target_capacities:
+        query_string = f'SELECT COUNT(*) FROM "default"."{TABLE_NAME}" WHERE SPS != -1 AND TargetCapacity = {target_capacity};'
+        result = query_athena(athena_client, query_string, result_configuration)
+        num_rows = result['ResultSet']['Rows'][1]['Data'][0]['VarCharValue']
+        message += f"\n\tTarget Capacity {target_capacity:2} : {num_rows}"
+
+    # Total Number of Row which Collected yesterday data
+    query_string = f'SELECT COUNT(*) FROM "default"."{TABLE_NAME}" WHERE SPS != -1;'
+    result_sps = query_athena(athena_client, query_string, result_configuration)
+    num_rows = result_sps['ResultSet']['Rows'][1]['Data'][0]['VarCharValue']
+    message += f"\nTotal number of row collected sps data : {num_rows}```"
 
     return message
 
 def lambda_handler(event, context):
-    athena_client = boto3.client('athena', region_name='us-east-1')
+    athena_client = boto3.client('athena', region_name='us-west-2')
     create_athena_table(athena_client, 'sps-query-data')
     result_message = make_message(athena_client)
     print(result_message)
@@ -118,3 +132,17 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(message_json)
     }
+
+if __name__ == "__main__":
+    import os
+    session = boto3.session.Session(
+        aws_access_key_id = os.environ.get("AWS_ACCESS_KEY"),
+        aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    )
+    athena_client = session.client('athena', region_name='us-west-2')
+    create_athena_table(athena_client, 'sps-query-data')
+    result_message = make_message(athena_client)
+    print(f"---- raw ----\n{result_message}")
+    message_json = send_slack_message(result_message)
+    drop_athena_table(athena_client)
+    print(message_json)
--- collector/spot-dataset/aws/sps_collector/monitoring/slack_msg_sender.py
+++ collector/spot-dataset/aws/sps_collector/monitoring/slack_msg_sender.py
@@ -11,22 +11,20 @@ def send_slack_message(msg):
     module_name = inspect.stack()[1][1]
     line_no = inspect.stack()[1][2]
     function_name = inspect.stack()[1][3]
-
-    message = f"File \"{module_name}\", line {line_no}, in {function_name} :\n{msg}"
-
+    
     slack_data = {
-        "text": message
+        "text": msg
     }
 
-    requests.post(url, json=slack_data)
+    return requests.post(url, json=slack_data)
 
 
 def get_webhook_url():
     try:
         ssm = boto3.client('ssm', region_name='us-west-2')
-        parameter = ssm.get_parameter(Name="error_notification_slack_webhook_url", WithDecryption=False)
+        parameter = ssm.get_parameter(Name="test_workspace_slack_webhook_url", WithDecryption=False)
         url = parameter['Parameter']['Value']
     except:
-        url = os.environ.get('error_notification_slack_webhook_url')
+        url = os.environ.get('test_workspace_slack_webhook_url')
 
     return url

f9c497248f1f81654b3729f0419ff71533cbddfd
MyPC <133>
2025-01-26 16:38:36 +0900

    1. global update_available_locations변수 제거, 2. json 파일 read write 함수를 패키징. 3. location manager 의 함수들 재정리.

--- collector/spot-dataset/azure/lambda/current_collector/sps_location_manager.py
+++ collector/spot-dataset/azure/lambda/current_collector/sps_location_manager.py
@@ -6,8 +6,6 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
-updated_available_locations = True
-
 load_dotenv('./files_sps/.env')
 LOCATIONS_CALL_HISTORY_FILENAME = os.getenv('LOCATIONS_CALL_HISTORY_FILENAME')
 LOCATIONS_OVER_LIMIT_FILENAME = os.getenv('LOCATIONS_OVER_LIMIT_FILENAME')
@@ -18,51 +16,75 @@ def load_over_limit_locations():
     이 메서드는 구독 해당한 초과 locations를 로드하고, 1시간이 지났으며 유효하지 않은 항목을 제거합니다.
     over_limit_locations의 초기화는 ACCOUNTS_CONFIG 에서 구독을 읽어 이용합니다.
     '''
-    try:
-        try:
-            with open(LOCATIONS_OVER_LIMIT_FILENAME, 'r', encoding="utf-8") as file:
-                all_over_limit_locations = json.load(file)
+    updated_over_limit_locations_flag = False
+    all_over_limit_locations = sps_shared_resources.read_json_file(LOCATIONS_OVER_LIMIT_FILENAME)
 
-        except FileNotFoundError:
-            print("load_over_limit_locations func. Too_many_req_locations file not found; initializing.")
-            all_over_limit_locations = {}
-
-        except json.JSONDecodeError as e:
-            print(f"load_over_limit_locations func. Failed to parse JSON: {e}")
-            all_over_limit_locations = {}
-
-        except Exception as e:
-            print(f"load_over_limit_locations func. An unexpected error occurred: {e}")
-            all_over_limit_locations = {}
-
-        for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
-            if account_id not in all_over_limit_locations:
-                all_over_limit_locations[account_id] = {}
-            if "subscription_ids" in account_data:
-                for subscription_id in account_data['subscription_ids']:
-                    if subscription_id not in all_over_limit_locations[account_id]:
-                        all_over_limit_locations[account_id][subscription_id] = {}
-
-                    one_hour_ago = datetime.now() - timedelta(hours=1)
-                    for location_key, location_value in list(
-                            all_over_limit_locations[account_id][subscription_id].items()):
-                        try:
-                            dt = datetime.fromisoformat(location_value)
-                            if dt <= one_hour_ago:
-                                del all_over_limit_locations[account_id][subscription_id][location_key]
-                                print(
-                                    f"Deleted expired location: '{location_key}', subscription_id: '{subscription_id}'")
-
-                        except ValueError:
-                            print(f"Invalid isoformat string: '{location_value}', subscription_id: '{subscription_id}'")
-
-        with open(LOCATIONS_OVER_LIMIT_FILENAME, 'w') as file:
-            json.dump(all_over_limit_locations, file, indent=4)
-        return all_over_limit_locations
-
-    except Exception as e:
-        print(f"Failed to load_over_limit_locations: {e}")
+    if all_over_limit_locations is None:
+        print("Failed to load over limit locations. The file might be missing or corrupted.")
         return None
+
+    for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
+        if account_id not in all_over_limit_locations:
+            all_over_limit_locations[account_id] = {}
+        if "subscription_ids" in account_data:
+            for subscription_id in account_data['subscription_ids']:
+                if subscription_id not in all_over_limit_locations[account_id]:
+                    all_over_limit_locations[account_id][subscription_id] = {}
+
+                one_hour_ago = datetime.now() - timedelta(hours=1)
+                for location_key, location_value in list(
+                        all_over_limit_locations[account_id][subscription_id].items()):
+                    dt = datetime.fromisoformat(location_value)
+                    if dt <= one_hour_ago:
+                        updated_over_limit_locations_flag = True
+                        del all_over_limit_locations[account_id][subscription_id][location_key]
+                        print(f"Deleted expired location: '{location_key}', subscription_id: '{subscription_id}'")
+
+    if updated_over_limit_locations_flag:
+        if not sps_shared_resources.write_json_file(LOCATIONS_OVER_LIMIT_FILENAME, all_over_limit_locations):
+            print("Failed to save over_limit_location.")
+            return None
+    return all_over_limit_locations
+
+
+def check_and_add_available_locations():
+    '''
+    이 함수는 최신 사용 가능한 location을 수집하고, JSON 파일에 저장된 기존 구독 기록과 비교합니다.
+    새로운 위치가 발견되면 해당 구독 기록에 추가됩니다. 변경 사항이 있는 경우, 업데이트된 데이터를 JSON 파일에 저장합니다.
+    '''
+    added_available_locations_flag = False
+    available_locations = collect_available_locations()
+    if available_locations is None:
+        print("Failed to check_and_add_available_locations. No available locations collected.")
+        return False
+
+    all_subscriptions_history = sps_shared_resources.read_json_file(LOCATIONS_CALL_HISTORY_FILENAME) or {}
+
+    for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
+        if account_id not in all_subscriptions_history:
+            all_subscriptions_history[account_id] = {}
+        if "subscription_ids" in account_data:
+            for subscription_id in account_data['subscription_ids']:
+                if subscription_id not in all_subscriptions_history[account_id]:
+                    all_subscriptions_history[account_id][subscription_id] = {}
+
+                subscription_history = all_subscriptions_history[account_id][subscription_id]
+
+                for location in available_locations:
+                    if location not in subscription_history:
+                        print(f"Location: {location} is fresh for subscription {subscription_id}, will add it.")
+                        subscription_history[location] = []
+                        added_available_locations_flag = True
+
+    if added_available_locations_flag:
+        if not sps_shared_resources.write_json_file(LOCATIONS_CALL_HISTORY_FILENAME, all_subscriptions_history):
+            print("Failed to save available_locations to all_subscriptions_history file.")
+            return False
+        print("Successfully saved available_locations to all_subscriptions_history file.")
+        return True
+
+    print("available_locations has not been added.")
+    return False
 
 
 def load_call_history_locations():
@@ -70,67 +92,37 @@ def load_call_history_locations():
     이 메서드는 호출 이력을 로드하고 필요에 따라 초기화합니다.
     또한, 유효하지 않은 위치를 정리하고 호출 이력 파일을 업데이트합니다.
     '''
-    global updated_available_locations
-    try:
-        try:
-            with open(LOCATIONS_CALL_HISTORY_FILENAME, 'r', encoding="utf-8") as file:
-                all_subscriptions_history = json.load(file)
+    updated_history_flag = False
+    all_subscriptions_history = sps_shared_resources.read_json_file(LOCATIONS_CALL_HISTORY_FILENAME) or None
 
-        except FileNotFoundError:
-            print("load_call_history_locations func. file not found; initializing new history.")
-            all_subscriptions_history = {}
-
-        except json.JSONDecodeError as e:
-            print(f"load_call_history_locations func. Failed to parse JSON: {e}. Reinitializing history.")
-            all_subscriptions_history = {}
-
-        except Exception as e:
-            print(f"load_call_history_locations func. An unexpected error occurred: {e}")
-            all_subscriptions_history = {}
-
-        if updated_available_locations:
-
-            available_locations = get_available_locations()
-            if available_locations is None:
-                return None
-            updated_available_locations = False
-
-            for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
-                if account_id not in all_subscriptions_history:
-                    all_subscriptions_history[account_id] = {}
-                if "subscription_ids" in account_data:
-                    for subscription_id in account_data['subscription_ids']:
-                        if subscription_id not in all_subscriptions_history[account_id]:
-                            all_subscriptions_history[account_id][subscription_id] = {}
-
-                        subscription_history = all_subscriptions_history[account_id][subscription_id]
-
-                        for location in available_locations:
-                            if location not in subscription_history:
-                                print(f"Location: {location} is fresh for subscription {subscription_id}, will add it.")
-                                subscription_history[location] = []
-
-        one_hour_ago = datetime.now() - timedelta(hours=1)
-        for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
-            if "subscription_ids" in account_data:
-                for subscription_id in account_data['subscription_ids']:
-                    subscription_data = all_subscriptions_history.get(account_id, {}).get(subscription_id, {})
-
-                    all_subscriptions_history[account_id][subscription_id] = {
-                        location: [
-                            t for t in timestamps if datetime.fromisoformat(t) > one_hour_ago
-                        ]
-                        for location, timestamps in subscription_data.items()
-                    }
-
-        with open(LOCATIONS_CALL_HISTORY_FILENAME, 'w') as file:
-            json.dump(all_subscriptions_history, file, indent=4)
-
-        return all_subscriptions_history
-
-    except Exception as e:
-        print(f"Failed to load_call_history: {e}")
+    if all_subscriptions_history is None:
+        print("Failed to load call history. The file might be missing or corrupted.")
         return None
+
+    one_hour_ago = datetime.now() - timedelta(hours=1)
+    for account_id, account_data in sps_shared_resources.ACCOUNTS_CONFIG.items():
+        if "subscription_ids" in account_data:
+            for subscription_id in account_data['subscription_ids']:
+                subscription_data = all_subscriptions_history.get(account_id, {}).get(subscription_id, {})
+
+                new_subscription_data = {
+                    location: [
+                        t for t in timestamps if datetime.fromisoformat(t) > one_hour_ago
+                    ]
+                    for location, timestamps in subscription_data.items()
+                }
+
+                if new_subscription_data != all_subscriptions_history.get(account_id, {}).get(subscription_id, {}):
+                    updated_history_flag = True
+
+                all_subscriptions_history.setdefault(account_id, {})[subscription_id] = new_subscription_data
+
+    if updated_history_flag:
+        if not sps_shared_resources.write_json_file(LOCATIONS_CALL_HISTORY_FILENAME, all_subscriptions_history):
+            print("Failed to save the call history file with deleted expired values.")
+            return None
+
+    return all_subscriptions_history
 
 
 def update_call_history(account_id, subscription_id, location, current_history, all_subscriptions_history):
@@ -144,9 +136,7 @@ def update_call_history(account_id, subscription_id, location, current_history, 
         current_history[location].append(current_timestamp)
         all_subscriptions_history[account_id][subscription_id] = current_history
 
-        with open(LOCATIONS_CALL_HISTORY_FILENAME, 'w') as file:
-            json.dump(all_subscriptions_history, file, indent=4)
-
+        sps_shared_resources.write_json_file(LOCATIONS_CALL_HISTORY_FILENAME, all_subscriptions_history)
         return True
 
     except Exception as e:
@@ -159,20 +149,14 @@ def validation_can_call(location, history, over_limit_locations):
     이 메서드는 지정된 location으로 호출 가능한지 확인합니다.
     초과 요청 여부와 호출 이력의 크기를 기준으로 판단합니다.
     """
-    try:
-        if over_limit_locations is not None:
-            if ((location not in over_limit_locations)
-                    and (len(history[location]) < 10)):
-                return True
-        else:
-            if len(history[location]) < 10:
-                return True
-        return False
-
-
-    except Exception as e:
-        print(f"Failed to valid can_call: {e}")
-        return False
+    if over_limit_locations is not None:
+        if ((location not in over_limit_locations)
+                and (len(history[location]) < 10)):
+            return True
+    else:
+        if len(history[location]) < 10:
+            return True
+    return False
 
 
 def get_next_available_location():
@@ -239,8 +223,7 @@ def update_over_limit_locations(account_id, subscription_id, location, all_over_
         print("Save over_limit_locations. Account: " + account_id + ", Subscription ID:" + subscription_id.split('-')[
             0] + ", Location:", location + ", Time:", now.strftime('%Y-%m-%d %H:%M:%S'))
 
-        with open(LOCATIONS_OVER_LIMIT_FILENAME, 'w') as file:
-            json.dump(all_over_limit_locations, file, indent=4)
+        sps_shared_resources.write_json_file(LOCATIONS_OVER_LIMIT_FILENAME, all_over_limit_locations)
         return True
 
     except Exception as e:
@@ -248,11 +231,11 @@ def update_over_limit_locations(account_id, subscription_id, location, all_over_
         return False
 
 
-def get_available_locations():
+def collect_available_locations():
     """
     이 메서드는 사용 가능한 위치를 잘못된 파라미터로 Azure API를 호출해 리턴한 지원되는 위치 목록을 리턴합니다.
     """
-    print("Start to get_available_locations")
+    print("Start to collect_available_locations")
     id_1 = sps_shared_resources.ACCOUNTS_CONFIG['account_1']['subscription_ids'][0]
     location = "ERROR_LOCATION"
     request_body = {
@@ -284,5 +267,5 @@ def get_available_locations():
             return available_locations
 
     except Exception as e:
-        print(f"Failed to get_available_locations, Error: " + e)
+        print(f"Failed to collect_available_locations, Error: " + e)
         return None

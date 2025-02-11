3c655733d723e8dc7dfc2a57eff76183dcb9ee76
TaeYoon <tykim7@kookmin.ac.kr>
2025-02-12 00:55:27 +0900

    Fix shell script

--- collector/spot-dataset/aws/collector/server/sps/start_query.sh
+++ collector/spot-dataset/aws/collector/server/sps/start_query.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-export PYTHONPATH=/home/ubuntu/.local/lib/python3.11/site-packages
-export AWS_CONFIG_FILE=/home/ubuntu/.aws/config
-export AWS_SHARED_CREDENTIALS_FILE=/home/ubuntu/.aws/credentials
-
 current_date=$(date -u '+%Y-%m-%dT%H:%M')
 echo "Cron Job Executed at $current_date" > /home/ubuntu/cron_test.log
 
-python3 /home/ubuntu/collect_sps.py --timestamp "$current_date" > /home/ubuntu/cron_output.log 2>&1
+python3 /home/ubuntu/spotlake/collector/spot-dataset/aws/collector/server/sps/collect_sps.py --timestamp "$current_date" > /home/ubuntu/cron_output.log 2>&1

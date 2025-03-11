2b5f9c62b64f7f61d516527d68a0ad05a592f92d
Taeyang-Kim <taiyang@kookmin.ac.kr>
2025-03-12 00:49:24 +0900

    Azure collector, IF정보가 없을때의 slack alert를 임시로 logger.error로 변경.

--- collector/spot-dataset/azure/lambda/current_collector/lambda_function.py
+++ collector/spot-dataset/azure/lambda/current_collector/lambda_function.py
@@ -4,7 +4,7 @@ from load_if import load_if
 from load_price import collect_price_with_multithreading
 from utils.merge_df import merge_price_saving_if_df
 from utils.upload_data import update_latest_price_saving_if, save_raw_price_saving_if
-from utils.pub_service import send_slack_message
+from utils.pub_service import send_slack_message, Logger
 
 def lambda_handler(event, _):
     event_time_utc = event.get("time")
@@ -12,33 +12,43 @@ def lambda_handler(event, _):
 
     is_price_fetch_success = True
     is_if_fetch_success = True
+    if_df = price_saving_df = None
 
     # collect azure price data with multithreading
     try:
         price_saving_df = collect_price_with_multithreading()
+        if not price_saving_df:
+            is_price_fetch_success = False
+            error_msg = """AZURE PRICE MODULE EXCEPTION!\n price_saving_df is None"""
+
     except Exception as e:
-        result_msg = """AZURE PRICE MODULE EXCEPTION!\n %s""" % (e)
-        data = {'text': result_msg}
-        send_slack_message(result_msg)
         is_price_fetch_success = False
+        error_msg = """AZURE PRICE MODULE EXCEPTION!\n %s""" % (e)
+        data = {'text': error_msg}
 
     try:
         if_df = load_if()
+        if not if_df:
+            is_if_fetch_success = False
     except Exception as e:
-        result_msg = """AZURE IF MODULE EXCEPTION!\n %s""" % (e)
-        data = {'text': result_msg}
-        send_slack_message(result_msg)
+        error_msg = """AZURE IF MODULE EXCEPTION!\n %s""" % (e)
+        data = {'text': error_msg}
+        send_slack_message(error_msg)
         is_if_fetch_success = False
 
     if is_price_fetch_success and is_if_fetch_success:
         join_df = merge_price_saving_if_df(price_saving_df, if_df)
     elif not is_price_fetch_success and is_if_fetch_success:
         join_df = if_df
+        send_slack_message(error_msg)
+
     elif is_price_fetch_success and not is_if_fetch_success:
         price_saving_df['IF'] = -1.0
         current_df = price_saving_df[
             ['InstanceTier', 'InstanceType', 'Region', 'OndemandPrice', 'SpotPrice', 'Savings', 'IF']]
         join_df = current_df
+        Logger.error("Failed: if_fetch!")
+
     else:
         result_msg = """AZURE PRICE MODULE AND IF MODULE EXCEPTION!"""
         data = {'text': result_msg}

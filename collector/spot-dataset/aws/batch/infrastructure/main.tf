699dedb85fea0b966b8959a870898b0bd2693be0
황재일 <hwangjaeil@hwangjaeil-ui-MacBookAir.local>
2025-12-05 14:08:46 +0900

    Change Instnace Types pool

--- collector/spot-dataset/aws/batch/infrastructure/main.tf
+++ collector/spot-dataset/aws/batch/infrastructure/main.tf
@@ -201,7 +201,7 @@ resource "aws_batch_compute_environment" "spot_compute_env" {
     min_vcpus = 0
     desired_vcpus = 0
     
-    instance_type = ["t4g.micro", "t3.micro", "t3a.micro", "t4g.small", "t3.small", "t3a.small"]
+    instance_type = ["a1.medium", "c1.medium", "c6gd.medium", "c6gn.medium", "c7a.medium", "c7g.medium", "c7gd.medium", "c7gn.medium", "c8g.medium", "c8gd.medium", "c8gn.medium", "is4gen.medium", "m1.medium", "m3.medium", "m8a.medium", "m8g.medium", "m8gd.medium", "r7g.medium", "r7gd.medium", "r8a.medium", "r8g.medium", "r8gb.medium", "r8gd.medium", "r8gn.medium", "x2gd.medium"]
     
     subnets = var.subnet_ids
     security_group_ids = var.security_group_ids

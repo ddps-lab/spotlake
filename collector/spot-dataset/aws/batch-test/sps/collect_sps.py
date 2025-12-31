8d1fcc7452fc1d71095e138feecaccf99791d3e3
황재일 <hwangjaeil@hwangjaeil-ui-MacBookAir.local>
2025-12-31 20:54:31 +0900

    Recover Error Catcher

--- collector/spot-dataset/aws/batch-test/sps/collect_sps.py
+++ collector/spot-dataset/aws/batch-test/sps/collect_sps.py
@@ -185,6 +185,9 @@ def main():
 
         def get_next_credential(self):
             with self.lock:
+                if self.current_index >= self.max_index:
+                    raise IndexError("No more credentials available in the current set.")
+                
                 credential = self.credentials.iloc[self.current_index]
                 # Use modulo to wrap index back to 0 when it reaches max
                 self.current_index = (self.current_index + 1) % self.max_index
